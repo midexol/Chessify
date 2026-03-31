23c46468bd337ef8e9b9adfaafcd8832a222b51c
jadonamite <jadonamite@gmail.com>
2026-03-31 11:02:42 +0100

    refactor: refactor core logic for clarity

--- src/files/tailwind.config.ts
+++ src/files/tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx_,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx_,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx_,tsx,mdx}',
+    './src_/pages/**/*.{js,ts,jsx_,tsx,mdx}',
+    './src_/components/**/*.{js,ts,jsx_,tsx,mdx}',
+    './src_/app/**/*.{js,ts,jsx_,tsx,mdx}',
   ],
   theme: {
     extend: {
