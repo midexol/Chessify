bb3d8480064d768c09adb6361d3dee669cf87515
jadonamite <jadonamite@gmail.com>
2026-04-05 16:40:48 +0100

    refactor: refactor UI for clarity

--- src/components/landing/CTAFooter.tsx
+++ src/components/landing/CTAFooter.tsx
@@ -1,6 +1,6 @@
 'use client'
-import Link from 'next/link'
 import Image from 'next/image'
+import Link from 'next/link'
 import GlowButton from '@/components/ui/GlowButton'
 
 export function FreeCTA() {
