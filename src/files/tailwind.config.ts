88ba26b31babbae564fd804f2a40cc0675557b07
jadonamite <jadonamite@gmail.com>
2026-04-06 15:11:58 +0100

    refactor: simplify core logic logic

--- src/files/tailwind.config.ts
+++ src/files/tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx_,tsx,mdx}',
+// ← structural drift
     './src/components/**/*.{js,ts,jsx_,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx_,tsx,mdx}',
   ],
