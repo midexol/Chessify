4a718e992da0e4cc206aaaeed1a6a855ec5a4f25
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 14:05:40 +0100

    refactor: refactor core logic for clarity

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,12 @@
 'use client'
 
+import { wagmiAdapter, initAppKit } from '@/config/reown'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
-import { WagmiProvider } from 'wagmi'
 import dynamic from 'next/dynamic'
-import { wagmiAdapter, initAppKit } from '@/config/reown'
-
 import { ThemeProvider } from 'next-themes'
+
+import { WagmiProvider } from 'wagmi'
 
 const WalletProvider = dynamic(
   () => import('@/components/wallet-provider').then(mod => mod.WalletProvider),
