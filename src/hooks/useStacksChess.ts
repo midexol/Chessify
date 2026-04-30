6a48ef2dc58f04ed234d959189358a4598e3cfd5
Mide_xol <okunolaolamide7@gmail.com>
2026-04-30 12:23:57 +0100

    refactor: simplify UI logic

--- src/hooks/useStacksChess.ts
+++ src/hooks/useStacksChess.ts
@@ -1,14 +1,14 @@
 'use client'
 
-import { useCallback } from 'react'
+import { useWallet } from '@/components/wallet-provider'
 import { 
   AnchorMode, 
   PostConditionMode, 
   uintCV,
   Pc
 } from '@stacks/transactions'
-import { useWallet } from '@/components/wallet-provider'
 import { STACKS_CONTRACTS, TOKEN_DECIMALS } from '@/config/contracts'
+import { useCallback } from 'react'
 
 export function useStacksChess() {
   const { stacksAddress, isStacksConnected, userSession } = useWallet()
